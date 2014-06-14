16368bf18cec01ee5548bd048b2e6adc67d3eadc
Kevin Lin <whatakevin@gmail.com>
2014-06-14 16:00:22 -0500

    start routing

--- js/app.js
+++ js/app.js
@@ -1,2 +1,5 @@
-angular.module("cncApp", ["ngRoute"]);
+var cncApp = angular.module('cncApp', ['ngRoute']);
 
+cncApp.config(['$routeProvider', function($routeProvider){
+  $routeProvider.when('/home', {templateUrl: 'partials/home.html'});
+}]);
